806fcd1d6dbaf274b2f281a49cabcacfecae3e21
Krovannox <krovannox@proton.me>
2026-03-24 10:29:25 -0500

    Add functions for the dialog element
    Some other functions where reordered for better readability

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// Lots of comments added for my learning process
+///////////////////
+// LIBRARY ARRAY //
+///////////////////
 
-// Main array, it should start empty at the beginning of the page
 const myLibrary = [];
 
-// The constructor handles only the creation of book objects with all the data it should need
+//////////////////////
+// BOOK CONSTRUCTOR //
+//////////////////////
+
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
@@ -12,34 +16,94 @@ function Book(title, author, pages) {
     this.bookID = crypto.randomUUID(); // BookID generated at it's creation
 }
 
-// This functions only adds books to the main array using the constructor Book
+/////////////////////////
+// FUNCTION - ADD BOOK //
+/////////////////////////
+
 function addBook(title, author, pages) {
     return myLibrary.push(new Book(title, author, pages));
 }
 
-// This function only removes book, not only from the DOM but from the main array, so we don't get leftover data
+///////////////////////////
+//FUNCTION - REMOVE BOOK //
+///////////////////////////
+
 function removeBook(bookID) {
     const index = myLibrary.findIndex(book => book.bookID === bookID);
     if (index !== -1) return myLibrary.splice(index, 1);
 }
 
-// Get the container for use in event delegation
+///////////////////////////////////
+// GET DIALOG AND INPUT ELEMENTS //
+///////////////////////////////////
+
+const dialogInputForm = document.getElementById("dialog-container");
+const bookTitle = document.getElementById("book-title");
+const bookAuthor = document.getElementById("book-author");
+const bookPages = document.getElementById("book-pages");
+
+/////////////////////////////////////////
+// FUNCTION - CLEAR DIALOG FORM INPUTS //
+/////////////////////////////////////////
+
+function clearFormInputs(dialogInputForm) {
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    bookPages.value = "";
+}
+
+//////////////////////////////////
+// FUNCTION - CLOSE DIALOG FORM //
+//////////////////////////////////
+
+function closeDialog(targetFormBtn) {
+    dialogInputForm.close();
+}
+
+///////////////////////////////////////////
+// EVENT LISTENER FOR "+ADD BOOK" BUTTON //
+///////////////////////////////////////////
+
+const addBookBtn = document.getElementById("btn-add-book");
+
+addBookBtn.addEventListener("click", function(e) {
+    clearFormInputs(dialogInputForm); // Clears the form
+    dialogInputForm.showModal(); // Open the form
+})
+
+//////////////////////////////////////////////
+// EVENT LISTENER FOR DIALOG FORM CONTAINER //
+//////////////////////////////////////////////
+
+dialogInputForm.addEventListener("click", function(e) {
+    const targetFormBtn = e.target;
+
+    // Delegation for the "Close" button
+    if (targetFormBtn.id === "btn-close-add-book-form") closeDialog(targetFormBtn);
+})
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ORDER PENDING//
+
+/////////////////////////
+// GET BOOKS CONTAINER //
+/////////////////////////
+
 const bookshelf = document.getElementById("books-container");
 
-// Event delegation function
+// Event delegation function for the books container
 bookshelf.addEventListener("click", function(e) {
-    const targetBtn = e.target;
+    const targetBookshelfBtn = e.target;
 
-    // Listener for the Read/Unread button
-    if (targetBtn.classList.contains("btn-read-status")) {
-        toggleReadUnreadDisplay(targetBtn);
-    }
+    // Delegation for the Read/Unread button
+    if (targetBookshelfBtn.classList.contains("btn-read-status")) toggleReadUnreadDisplay(targetBookshelfBtn)
 })
 
 // Function that handles the toggle and change of text of the Read/Unread button
-function toggleReadUnreadDisplay(targetBtn) {
-    targetBtn.classList.toggle("btn-read-status-read");
-    targetBtn.textContent = targetBtn.classList.contains("btn-read-status-read")
+function toggleReadUnreadDisplay(targetBookshelfBtn) {
+    targetBookshelfBtn.classList.toggle("btn-read-status-read");
+    targetBookshelfBtn.textContent = targetBookshelfBtn.classList.contains("btn-read-status-read")
         ? "Read"
         : "Unread";
 }
